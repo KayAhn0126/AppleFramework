5c1bfe7674c224dd87c68bcb58caa284e5f954cb
KayAhn0126 <dkseotn123@gmail.com>
2022-08-18 13:34:27 +0900

    refactor: 기존 view Controller 삭제 후 FrameworkViewController 추가

--- AppleFramework/AppleFramework/FrameworkViewController.swift
+++ AppleFramework/AppleFramework/FrameworkViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  FrameworkViewController.swift
 //  AppleFramework
 //
 //  Created by Kay on 2022/08/18.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class FrameworkViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        
     }
-
-
 }
-
