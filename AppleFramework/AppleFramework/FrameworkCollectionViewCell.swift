461861419f64710f1bdb6a6806553657f5c88cfc
KayAhn0126 <dkseotn123@gmail.com>
2022-08-18 14:53:55 +0900

    refactor: collection view와 cell 구성 추가

---
+++ AppleFramework/AppleFramework/FrameworkCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  FrameworkCollectionViewCell.swift
+//  AppleFramework
+//
+//  Created by Kay on 2022/08/18.
+//
+
+import UIKit
+
+class FrameworkCollectionViewCell: UICollectionViewCell {
+    
+}
